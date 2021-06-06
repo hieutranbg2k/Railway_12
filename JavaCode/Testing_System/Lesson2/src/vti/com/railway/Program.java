b08c5df50230cf40a4cae9e186e7765cecfc6095
hieutranbg2k <hieutranbg2k@gmail.com>
2021-06-06 10:08:46 +0700

    lesson 1

---
+++ JavaCode/Testing_System/Lesson2/src/vti/com/railway/Program.java
@@ -0,0 +1,11 @@
+package vti.com.railway;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+
+	}
+
+}
